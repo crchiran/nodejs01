89ef331f144f913718fba86a45e867ccc9e2bf33
crchiran <datatigerbd@gmail.com>
2022-10-30 23:27:35 +0600

    add

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo scp index.js 170.187.228.173:/srv/nodejs01
-sudo pm2 stop index.js
-sudo npm install
-sudo npm run build
-sudo pm2 start index.js
+scp index.js 170.187.228.173:/srv/nodejs01/
+pm2 stop index.js
+npm install
+npm run build
+pm2 start index.js
